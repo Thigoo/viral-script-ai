c23864ee49f8639051bf20549f9a20c530d882a0
Thigoo <thiagomvk08@gmail.com>
2026-02-24 18:45:28 -0300

    chore: splits ai models for each creative and critc

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 API_KEY = os.getenv('AI_API_KEY')
 BASE_URL = 'https://openrouter.ai/api/v1'
-AI_MODEL = 'arcee-ai/trinity-large-preview:free'
+CREATIVE_AI_MODEL = 'arcee-ai/trinity-large-preview:free'
+CRITIC_AI_MODEL = 'liquid/lfm-2.5-1.2b-thinking:free'
 
 client = OpenAI(
     api_key = API_KEY,
